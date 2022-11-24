a1ff218ba548d9ba95ef71731a0b928068eb65ab
Nechaev-Git <nechaev_arkadij@mail.ru>
2022-11-24 13:53:53 +0300

    add get_all_child_process func

--- statistics_in_1sec.py
+++ statistics_in_1sec.py
@@ -8,13 +8,26 @@ statistics = {}
 
 monitoring_files_path = "/opt/rubackup/monitoring/rubackup-stress.rubackup.local_db7963975bdae884/"
 
+for proc in psutil.process_iter():
+        if 'rubackup_client' in proc.name():
+            pid = proc.pid
+
+def pid_io_stat():
+    with open(f'/proc/{pid}/io') as pid_io_file:
+        stats = pid_io_file.readlines()
+
 def net_usage(inf = "ens18"):   #change the inf variable according to the interface
     net_stat = psutil.net_io_counters(pernic=True, nowrap=True)[inf]
     net_in = net_stat.bytes_recv
     net_out = net_stat.bytes_sent
 
-    net_usage_stats = {'net_recieved': net_in, 'net_sent': net_out}
+    with open(f'/proc/{pid}/net/dev') as net_dev_file:
+        net_dev_stats = net_dev_file.read()
+        net_client_recieve_bytes = net_dev_stats.split()[21]
+        net_client_transmit_bytes = net_dev_stats.split()[28]
 
+    net_usage_stats = {'net_recieved': net_in, 'net_sent': net_out, 'net_client_usage_w': int(net_client_transmit_bytes), 'net_client_usage_r': int(net_client_recieve_bytes)}
+    
     return net_usage_stats
 
 def b_to_m(b):
@@ -22,6 +35,7 @@ def b_to_m(b):
     return m
 
 while True:
+    #print(pid_io_stat())
     iostat_call = subprocess.Popen(['iostat','-d','-t','-y','-o','JSON','1','1'], stdout=subprocess.PIPE)
     mpstat_call = subprocess.Popen(['mpstat','-o','JSON','1','1'], stdout=subprocess.PIPE)
     pidstat_call = subprocess.Popen(['pidstat','-h','-u','-r','-I','-C','rubackup_client','1','1'], stdout=subprocess.PIPE)
@@ -54,21 +68,21 @@ while True:
     client_memory_usage_m = b_to_m(client_memory_usage)
 
     net_usage_output = net_usage()
-
+    
     current_time = subprocess.getoutput('date +%a\ %b\ %e\ %T\ %G')
     file_name = subprocess.getoutput('date +%F-%k-%M-%S')
     
     statistics[file_name] = {'disk_io_usage': {'iostat_timestamp': iostat_timestamp, 'vda_io_usage_wrtn': vda_io_usage_wrtn, 'vda_io_usage_read': vda_io_usage_read}, \
                              'net_usage_total': net_usage_output, \
-                             'net_usage_rates': {'net_recieved_KB': 0,'net_sent_KB': 0},\
+                             'net_usage_rates': {'net_recieved_KB': 0,'net_sent_KB': 0, 'net_client_recieved_KB': 0, 'net_client_sent_KB': 0} ,\
                              'cpu_usage': {'mpstat_timestamp': mpstat_timestamp,'cpu_load_usr': cpu_load_usr, 'client_cpu_load_usr': client_cpu_load_usr}, \
                              'memory_usage': {'general_memory_usage_m': general_memory_usage_m, 'general_memory_usage_percent': general_memory_usage_percent, 'client_memory_usage_percent': client_memory_usage_percent, 'client_memory_usage_m': client_memory_usage_m}, \
                              }
 
 
-    if len(statistics) < 5:
+    if len(statistics) < 60:
         continue
-    elif len(statistics) > 5:
+    elif len(statistics) > 60:
         statistics.pop(list(statistics.keys())[0])
     
     key_name = list(statistics.keys())[0]
@@ -78,7 +92,11 @@ while True:
     next_net_rates_in = (statistics[key_name_next]['net_usage_total']['net_recieved'] - statistics[key_name]['net_usage_total']['net_recieved']) / 1024
     next_net_rates_out = (statistics[key_name_next]['net_usage_total']['net_sent'] - statistics[key_name]['net_usage_total']['net_sent']) / 1024
 
-    statistics[key_name_next]['net_usage_rates'] = {'net_recieved_KB': next_net_rates_in, 'net_sent_KB': next_net_rates_out}
+    next_net_client_rates_in = (statistics[key_name_next]['net_usage_total']['net_client_usage_r'] - statistics[key_name]['net_usage_total']['net_client_usage_r']) / 1024
+    next_net_client_rates_out = (statistics[key_name_next]['net_usage_total']['net_client_usage_w'] - statistics[key_name]['net_usage_total']['net_client_usage_w']) / 1024
+    print(next_net_client_rates_out)
+    print(next_net_client_rates_in)
+    statistics[key_name_next]['net_usage_rates'] = {'net_recieved_KB': next_net_rates_in, 'net_sent_KB': next_net_rates_out, 'net_client_recieved_KB': next_net_client_rates_in, 'net_client_sent_KB': next_net_client_rates_out}
 
     try:
         with open(file_path,'r') as j:
@@ -94,15 +112,18 @@ while True:
             print('general_io_usage_r'+' '+monitoring_file_data['general_io_usage_r'])
             print('iostat_general_io_usage_r'+' '+str(statistics[key_name]['disk_io_usage']['vda_io_usage_read']))
             print('client_io_usage_r'+' '+monitoring_file_data['client_io_usage_r'])
+            print('client_io_usage_w'+' '+monitoring_file_data['client_io_usage_w'])
             print('general_io_usage_w'+' '+monitoring_file_data['general_io_usage_w'])
             print('iostat_general_io_usage_w'+' '+str(statistics[key_name]['disk_io_usage']['vda_io_usage_wrtn']))
             print('\n')
             print('general_net_usage_w'+' '+monitoring_file_data['general_net_usage_w'])
             print('net_sent_KB'+' '+str(statistics[key_name]['net_usage_rates']['net_sent_KB']))
             print('client_net_usage_w'+' '+monitoring_file_data['client_net_usage_w'])
+            print('net_client_sent_KB'+' '+str(statistics[key_name]['net_usage_rates']['net_client_sent_KB']))
             print('general_net_usage_r'+' '+monitoring_file_data['general_net_usage_r'])
             print('net_recieved_KB'+' '+str(statistics[key_name]['net_usage_rates']['net_recieved_KB']))
             print('client_net_usage_r'+' '+monitoring_file_data['client_net_usage_r'])
+            print('net_client_recieved_KB'+' '+str(statistics[key_name]['net_usage_rates']['net_client_recieved_KB']))
             print('\n')
             print('general_ram_usage_%'+' '+monitoring_file_data['general_ram_usage'])
             print('general_memory_usage_%'+' '+str(statistics[key_name]['memory_usage']['general_memory_usage_percent']))
