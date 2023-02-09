fee95de5194ff300f39f6ee0e1f8163f55ceb311
me <nechaev_arkadij@mail.ru>
2023-02-09 21:39:43 +0300

    add comments and new func

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-import subprocess
+from subprocess import getoutput
 import psutil
 from collections import defaultdict
-import platform
+from platform import node
 import sys
+from os import listdir
 from datetime import datetime
 from time import sleep
 
@@ -13,11 +14,17 @@ net_interface = "ens18"
 disk = "sda"
 
 # Период мониторинга. Передаётся аргументом к скрипту
-monitoring_period = sys.argv[1]
+monitoring_period = int(sys.argv[1])
+# Необходимое количество записей
+records = int(sys.argv[2])
+# Количество итераций для необходимого количества записей
+iterations = monitoring_period * records
+
+print(f"{iterations} секунд необходимо для сбора статистики")
 
 # Из имени хоста и полученого hwid составляется путь до директории с файлами мониторинга
-hostname = platform.node()
-hwid = subprocess.getoutput("/opt/rubackup/bin/rubackup_client hwid").split("\n")[2]
+hostname = node()
+hwid = getoutput("/opt/rubackup/bin/rubackup_client hwid").split("\n")[2]
 monitoring_files_path = "/opt/rubackup/monitoring/" + hostname + "_" + hwid + "/"
 
 # Создание словарей для последующего размещения в них собранной статистики.
@@ -65,7 +72,7 @@ def collect_general_stats(timestamp):
         "memory_usage_percent": memory_usage_percent,
         "memory_usage_m": memory_usage_m,
     }
-    # Вызывается функция для расчёта показателей, если длина словаря > 2.
+    # Вызывается функция для расчёта показателей, если длина словаря >= 2
     if len(general_stats_counters) >= 2:
         calculate_general_stats(timestamp, general_stats_counters, general_stats)
 
@@ -79,7 +86,7 @@ def calculate_general_stats(timestamp, general_stats_counters, general_stats):
     io_read = general_stats_counters[timestamp]["io_read"]
     io_write = general_stats_counters[timestamp]["io_write"]
 
-    # Для получение показателей из предыдущей итерации получаем значение предыдущего ключа.
+    # Для получение показателей из предыдущей итерации получаем значение предыдущего ключа
     prev_timestamp = list(general_stats_counters.keys())[-2]
 
     # Расчёт дельты между показателями из текущей итерации и предыдущей
@@ -90,10 +97,12 @@ def calculate_general_stats(timestamp, general_stats_counters, general_stats):
     delta_io_read = io_read - general_stats_counters[prev_timestamp]["io_read"]
     delta_io_write = io_write - general_stats_counters[prev_timestamp]["io_write"]
 
+    # Расчёт показателей в процентах и мегабайтах
     cpu_usage_percent = (delta_total_use_cpu_time / delta_total_cpu_time) * 100
     memory_usage_percent = general_stats_counters[timestamp]["memory_usage_percent"]
     memory_usage_m = general_stats_counters[timestamp]["memory_usage_m"]
 
+    # Добавление показателей в словарь general_stats
     general_stats[timestamp] = {}
     general_stats[timestamp]["cpu_percent"] = cpu_usage_percent
     general_stats[timestamp]["net_in"] = delta_net_in
@@ -104,6 +113,8 @@ def calculate_general_stats(timestamp, general_stats_counters, general_stats):
     general_stats[timestamp]["memory_usage_m"] = memory_usage_m
 
 
+# Функция используется внутри функции calculate_client_total_stat и считает статистику для одного процесса
+# по всем показателям
 def get_clients_stats(proc_pid):
     try:
         child_proc = psutil.Process(proc_pid)
@@ -120,13 +131,18 @@ def get_clients_stats(proc_pid):
         return [0, 0, 0, 0]
 
 
+# Функция для расчёта суммы по каждому показателю для процесса rubackup_client и всех порожденных им процессов
 def calculate_client_total_stat(list_of_child_pids, timestamp):
 
+    # Функция map вызывает функцию get_clients_stats для каждого pid'а внутри списка list_of_child_pids
+    # и возвращает итератор, состоящий из списков([cpu,io_read,io_write,memory])
+    # со статистикой по каждому показателю для каждого pid'a
     list_of_stat = map(get_clients_stats, list_of_child_pids)
+    # Функция zip составляет итератор, содержащий статистику, скомпонованную отдельно для каждого показателя
     list_of_stat_zipped = zip(*list_of_stat)
-    # total_list = [sum(i) for i in list_of_stat_zipped]
+    # Функция map возвращает итератор, сотоящий из сумм значений каждого элемента в list_of_stat_zipped
     total_list = list(map(sum, list_of_stat_zipped))
-
+    # Значения из списка total_list помещаются в соответствующее место в словаре
     client_stats_counters[timestamp] = {
         "client_cpu_time": total_list[0],
         "client_io_read": total_list[1],
@@ -137,14 +153,14 @@ def calculate_client_total_stat(list_of_child_pids, timestamp):
 
 def collect_client_stats(timestamp):
     rubackup_client_pid = pid_and_childs_pids[0]
-
+    # Нахлждение всех процессов, порожденных процессом rubackup_client, и всех их дочерних процессов
     for children in psutil.Process(rubackup_client_pid).children(recursive=True):
         pid_and_childs_pids.append(children.pid)
 
     calculate_client_total_stat(pid_and_childs_pids, timestamp)
-
+    # Удаляем идентификаторы всех дочерних процессов из списка
     del pid_and_childs_pids[1:]
-
+    # Вызывается функция для расчёта показателей, если длина словаря >= 2
     if len(client_stats_counters) >= 2:
         total_cpu_time = general_stats_counters[timestamp]["total_cpu_time"]
         client_total_cpu_time = client_stats_counters[timestamp]["client_cpu_time"]
@@ -152,16 +168,18 @@ def collect_client_stats(timestamp):
         client_io_write = client_stats_counters[timestamp]["client_io_write"]
         total_memory_m = general_stats[timestamp]["memory_usage_m"]
 
+        # Для получение показателей из предыдущей итерации получаем значение предыдущего ключа
         prev_timestamp = list(client_stats_counters.keys())[-2]
+        # Расчёт дельты между показателями из текущей итерации и предыдущей
         delta_total_cpu_time = total_cpu_time - general_stats_counters[prev_timestamp]["total_cpu_time"]
         delta_client_cpu_time = client_total_cpu_time - client_stats_counters[prev_timestamp]["client_cpu_time"]
         delta_client_io_read = client_io_read - client_stats_counters[prev_timestamp]["client_io_read"]
         delta_client_io_write = client_io_write - client_stats_counters[prev_timestamp]["client_io_write"]
-
+        # Расчёт показателей в процентах и мегабайтах
         client_cpu_usage_percent = (delta_client_cpu_time / delta_total_cpu_time) * 100
         client_memory_m = client_stats_counters[timestamp]["client_memory"]
         client_memory_percent = (client_memory_m / total_memory_m) * 100
-
+        # Добавление показателей в словарь general_stats
         client_stats[timestamp] = {}
         client_stats[timestamp]["client_cpu_percent"] = client_cpu_usage_percent
         client_stats[timestamp]["client_io_read"] = delta_client_io_read
@@ -176,8 +194,18 @@ def collect_stats():
     collect_client_stats(timestamp)
 
 
-while True:
+def calculate_period_stats():
+    monitoring_files = listdir(monitoring_files_path)
+    period_stats = {}
+    for key in general_stats.keys():
+        if key in monitoring_files:
+            period_stats[key] = defaultdict
+    print(f"\n{period_stats}")
+
+
+while len(general_stats) <= iterations:
     collect_stats()
-    print("client_stats " + str(client_stats) + "\n")
-    print("general_stats " + str(general_stats) + "\n")
+    print(f"\rОсталось {iterations - len(general_stats)} секунд", end="")
     sleep(1)
+
+calculate_period_stats()
